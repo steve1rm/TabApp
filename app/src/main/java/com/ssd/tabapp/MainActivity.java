03b8f9eb008c30593c3fb81946e603d4f60451b1
Steve <steve1.rm@gmail.com>
2015-09-23 12:26:44 +0700

    Added custom tabs with text and icons

--- app/src/main/java/com/ssd/tabapp/MainActivity.java
+++ app/src/main/java/com/ssd/tabapp/MainActivity.java
@@ -20,8 +20,20 @@ public class MainActivity extends AppCompatActivity {
 
     private void setupTabs() {
         TabLayout tabLayout = (TabLayout)findViewById(R.id.tabLayout);
-        tabLayout.addTab(tabLayout.newTab().setText("Movies"));
+
+        TabLayout.Tab findTab = tabLayout.newTab();
+        findTab.setCustomView(R.layout.tab_search);
+        tabLayout.addTab(findTab);
+
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.movies));
         tabLayout.addTab(tabLayout.newTab().setText("Music"));
         tabLayout.addTab(tabLayout.newTab().setText("Library"));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_action_ic_search_white_24dp));
+        tabLayout.addTab(tabLayout.newTab().setText("Favourites"));
+        TabLayout.Tab searchTab = tabLayout.newTab();
+
+        searchTab.setIcon(R.drawable.ic_action_ic_search_white_24dp);
+        searchTab.setText("Find");
+        tabLayout.addTab(searchTab);
     }
 }
